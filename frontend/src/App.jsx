8cae6c22ec42a0258dc10c0f1a86461b7351ca47
Aarush <kraarush709@gmail.com>
2025-04-14 02:52:46 +0530

    table data updated dynamically

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,27 +7,71 @@ import { Loader2 } from "lucide-react";
 import TableCard from "./component/tableCard";
 
 const App = () => {
-  
   CustomHook();
 
   const opportunityData = useSelector((store) => store.opportunityData);
   const acvData = useSelector((store) => store.acvData);
   const loading = useSelector((store) => store.loading);
 
+  let totalAcvValue = 0;
+  let totalOpportunityValue = 0;
+
+
+  // setting the table data
+  const opportunityTableData = opportunityData.map((item,index,arr) => {
+    const came = item.count || 0;
+    const bottom = item.bottomValue ?? 0;
+    const moved = item.label === "Won" ? "-" : Math.ceil(came * (bottom / 100));
+    const lost = item.label === "Won" ? "-" : came - moved;
+    const winRate = `${item.rightPercentage}%`;
+    if (typeof lost === "number") totalOpportunityValue += lost;
+  
+    return {
+      stage: item.label,
+      came,
+      lost,
+      moved,
+      winRate,
+    };
+  });
+  
+
+  // setting the acv table data
+  const acvTableData = acvData.map((item, index) => {
+    const came = item.acv || 0;
+    const nextItem = acvData[index + 1];
+    const moved = item.label === "Won" || !nextItem ? "-" : Math.round(nextItem.acv) || 0;
+    const lost = item.label === "Won" ? "-" : came - moved;
+    const winRate = `${item.rightPercentage}%`;
+    if (typeof lost === "number") totalAcvValue += lost;
+  
+    return {
+      stage: item.label,
+      came,
+      lost,
+      moved,
+      winRate,
+    };
+  });
+
+  
   if (loading) {
     return (
       <div className="h-screen w-full flex items-center justify-center">
         <Loader2 className="animate-spin w-10 h-10 mr-2" /> loading Data
       </div>
-    )
+    );
   }
 
   return (
     <div className="max-w-[1400px] mx-auto flex flex-col gap-4 my-4 p-2 sm:gap-4 sm:my-10 sm:p-4">
-
       {/* top bar */}
       <div className="border-b-3 border-gray-300">
-        <ButtonGroup variant="contained" aria-label="Basic button group" size="small" >
+        <ButtonGroup
+          variant="contained"
+          aria-label="Basic button group"
+          size="small"
+        >
           <Button>Summary</Button>
           <Button variant="outline">What-if</Button>
         </ButtonGroup>
@@ -35,14 +79,14 @@ const App = () => {
 
       {/* cards for data visualization */}
       <div className="flex flex-col gap-4 sm:flex-row sm:gap-4 justify-center items-center">
-        <Card key={1} data={opportunityData} heading={'opportunity count'}/>
-        <Card key={2} data={acvData} heading={'ACV'}/>
+        <Card key={1} data={opportunityData} heading={"opportunity count"} />
+        <Card key={2} data={acvData} heading={"ACV"} />
       </div>
 
       {/* tables */}
       <div className="flex flex-col gap-4 sm:flex-row sm:gap-4 justify-center items-center">
-        <TableCard/>
-        <TableCard/>
+        <TableCard key={1} data={opportunityTableData} totalValue={totalOpportunityValue}/>
+        <TableCard key={2} data={acvTableData} totalValue={totalAcvValue}/>
       </div>
     </div>
   );
--- frontend/src/component/TableCard.jsx
+++ frontend/src/component/TableCard.jsx
@@ -2,27 +2,18 @@ import React from "react";
 import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { styled } from "@mui/material/styles";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
-import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 
-const TableCard = ({ clipboardText }) => {
-  const data = [
-    { stage: "Suspect", came: 24, lost: 6, moved: 18, winRate: "13%" },
-    { stage: "Qualify", came: 18, lost: 10, moved: 8, winRate: "17%" },
-    { stage: "Demo", came: 8, lost: 4, moved: 4, winRate: "38%" },
-    { stage: "Proposal", came: 4, lost: 1, moved: 3, winRate: "75%" },
-    { stage: "Negotiate", came: 3, lost: 0, moved: 3, winRate: "100%" },
-    { stage: "Won", came: 3, lost: "-", moved: "-", winRate: "100%" },
-  ];
-
+const TableCard = ({ data , totalValue}) => {
   const handleCopy = () => {
-    navigator.clipboard.writeText(clipboardText).then(() => {
+    navigator.clipboard.writeText(JSON.stringify(data,null,2))
+    .then(() => {
       toast.success("Copied to clipboard!", {
         position: "top-right",
         autoClose: 1500,
@@ -33,7 +24,7 @@ const TableCard = ({ clipboardText }) => {
   };
 
   return (
-    <div className="flex flex-col border border-gray-300 w-full shadow-xl gap-3 py-2 px-2 rounded-sm">
+    <div className="flex flex-col border border-gray-300 w-full shadow-xl gap-2 py-5 px-2 rounded-sm">
       {/* copy button div */}
       <div className="w-full flex justify-end text-gray-500 p-2">
         <div
@@ -45,60 +36,42 @@ const TableCard = ({ clipboardText }) => {
       </div>
 
       {/* Table div */}
-      <div className="p-2">
+      <div className="px-3">
         <TableContainer component={Paper}>
-          <Table sx={{ border: "1px solid #ccc", minWidth: "600px" }} size="small">
+          <Table sx={{ border: "1px solid #ccc", minWidth: "600px" }} size="small" >
+
             <TableHead>
               <TableRow>
-                <TableCell
-                  sx={{ borderRight: "1px solid #ccc", width: "140px" }}
-                  align="start"
-                >
+                <TableCell sx={{ borderRight: "1px solid #ccc", width: "135px" }} align="start" >
                   <b>Stage</b>
                 </TableCell>
-                <TableCell
-                  sx={{ borderRight: "1px solid #ccc" }}
-                  align="center"
-                >
+
+                <TableCell sx={{ borderRight: "1px solid #ccc", width: "110px" }} align="center" >
                   <b>Came to Stage</b>
                 </TableCell>
-                <TableCell
-                  sx={{
-                    backgroundColor: "#c65911",
-                    color: "white",
-                    borderRight: "1px solid #ccc",
-                    width: "180px",
-                  }}
-                  align="center"
-                >
+
+                <TableCell sx={{ backgroundColor: "#c65911", color: "white", borderRight: "1px solid #ccc", width: "157px", }} align="center" >
                   <b>Lost / Disqualified from Stage</b>
                 </TableCell>
-                <TableCell
-                  sx={{
-                    backgroundColor: "#70ad47",
-                    color: "white",
-                    borderRight: "1px solid #ccc",
-                  }}
-                  align="center"
-                >
+
+                <TableCell sx={{ backgroundColor: "#70ad47", color: "white", borderRight: "1px solid #ccc",width: "110px" }} align="center">
                   <b>Moved to next stage</b>
                 </TableCell>
-                <TableCell align="center">
+
+                <TableCell align="center" sx={{width: "110px"}}>
                   <b>Win Rate %</b>
                 </TableCell>
               </TableRow>
             </TableHead>
+
             <TableBody>
               {data.map((row, idx) => (
-                <TableRow
-                  key={row.stage}
-                  sx={{
-                    backgroundColor: idx % 2 === 1 ? "#f5f5f5" : "white",
-                  }}
-                >
+                <TableRow key={row.stage} sx={{ backgroundColor: idx % 2 === 1 ? "#f5f5f5" : "white", }} >
+
                   <TableCell sx={{ borderRight: "1px solid #ccc" }}>
                     {row.stage}
                   </TableCell>
+
                   <TableCell
                     align="right"
                     sx={{
@@ -110,78 +83,39 @@ const TableCard = ({ clipboardText }) => {
                   >
                     {row.came}
                   </TableCell>
-                  <TableCell
-                    align="right"
-                    sx={{ borderRight: "1px solid #ccc" }}
-                  >
+
+                  <TableCell align="right" sx={{ borderRight: "1px solid #ccc" }}>
                     {row.lost}
                   </TableCell>
-                  <TableCell
-                    align="right"
-                    sx={{ borderRight: "1px solid #ccc" }}
-                  >
+
+                  <TableCell align="right" sx={{ borderRight: "1px solid #ccc" }}>
                     {row.moved}
                   </TableCell>
+
                   <TableCell align="right">{row.winRate}</TableCell>
                 </TableRow>
               ))}
-              <TableRow sx={{ height: "10px" }}>
-                <TableCell
-                  sx={{
-                    borderTop: "1px solid #ccc",
-                    borderRight: "1px solid #ccc",
-                    fontWeight: "bold",
-                    paddingTop: "1px",
-                    paddingBottom: "1px",
-                    paddingLeft: "8px",
-                    paddingRight: "8px",
-                  }}
-                >
-                  Total
-                </TableCell>
-                <TableCell
-                  align="right"
-                  sx={{
-                    borderRight: "1px solid #ccc",
-                    borderTop: "1px solid #ccc",
-                    py: 1,
-                    px: 2,
-                  }}
-                >
-                  -
-                </TableCell>
-                <TableCell
-                  align="right"
-                  sx={{
-                    borderRight: "1px solid #ccc",
-                    borderTop: "1px solid #ccc",
-                    fontWeight: "bold",
-                    py: 1,
-                    px: 2,
-                  }}
-                >
-                  21
-                </TableCell>
-                <TableCell
-                  align="right"
-                  sx={{
-                    borderRight: "1px solid #ccc",
-                    borderTop: "1px solid #ccc",
-                    py: 1,
-                    px: 2,
-                  }}
-                >
-                  -
-                </TableCell>
-                <TableCell align="right" sx={{ borderTop: "1px solid #ccc" }}>
-                  -
-                </TableCell>
+
+
+
+              {/* last row of the table */}
+              <TableRow sx={{ height: "10px"}}>
+
+                <TableCell sx={{ fontWeight: "bold", borderRight: "1px solid #ccc" }} > Total </TableCell>
+
+                <TableCell align="right" sx={{ fontWeight: "bold", borderRight: "1px solid #ccc" }}> - </TableCell>
+
+                <TableCell align="right" sx={{ fontWeight: "bold", borderRight: "1px solid #ccc" }}> {totalValue} </TableCell>
+
+                <TableCell align="right" sx={{ fontWeight: "bold", borderRight: "1px solid #ccc" }}> - </TableCell>
+
+                <TableCell align="right" sx={{ fontWeight: "bold", borderRight: "1px solid #ccc" }}> - </TableCell>
               </TableRow>
+
             </TableBody>
           </Table>
         </TableContainer>
       </div>
-
       <ToastContainer />
     </div>
   );
