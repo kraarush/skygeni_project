6f4e9e14980017c3ce7ab5ce8cdbd468ed4e98f1
Aarush <kraarush709@gmail.com>
2025-04-14 10:12:00 +0530

    updated import statement for tablecard in app.jsx for netlify deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import { Button, ButtonGroup } from "@mui/material";
 import CustomHook from "./CustomHook";
 import { useSelector } from "react-redux";
 import { Loader2 } from "lucide-react";
-import TableCard from "./component/tableCard";
+import TableCard from "./components/TableCard";
 
 const App = () => {
   CustomHook();
