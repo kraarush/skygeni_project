d78e2f21b9f84d8dfc65096df355f1301a201576
Aarush <kraarush709@gmail.com>
2025-04-14 12:02:00 +0530

    added footer in the website

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import CustomHook from "./CustomHook";
 import { useSelector } from "react-redux";
 import { Loader2 } from "lucide-react";
 import TableCard from "./component/TableCard";
-
+import Footer from "./component/Footer";
 
 const App = () => {
   CustomHook();
@@ -13,12 +13,15 @@ const App = () => {
   const opportunityData = useSelector((store) => store.opportunityData);
   const acvData = useSelector((store) => store.acvData);
   const loading = useSelector((store) => store.loading);
-  const opportunityTableData = useSelector((store) => store.opportunityTableData.tableData);
+  const opportunityTableData = useSelector(
+    (store) => store.opportunityTableData.tableData
+  );
   const acvTableData = useSelector((store) => store.acvTableData.tableData);
-  const opportunityTotalValue = useSelector((store) => store.opportunityTableData.totalValue);
+  const opportunityTotalValue = useSelector(
+    (store) => store.opportunityTableData.totalValue
+  );
   const acvTotalData = useSelector((store) => store.acvTableData.totalValue);
 
-  
   if (loading) {
     return (
       <div className="h-screen w-full flex items-center justify-center">
@@ -28,30 +31,38 @@ const App = () => {
   }
 
   return (
-    <div className="max-w-[1400px] mx-auto flex flex-col gap-4 my-4 p-2 sm:gap-4 sm:my-10 sm:p-4">
-      {/* top bar */}
-      <div className="border-b-3 border-gray-300">
-        <ButtonGroup
-          variant="contained"
-          aria-label="Basic button group"
-          size="small"
-        >
-          <Button>Summary</Button>
-          <Button variant="outline">What-if</Button>
-        </ButtonGroup>
+    <div>
+      <div className="max-w-[1400px] mx-auto flex flex-col gap-4 my-4 p-2 sm:gap-4 sm:my-10 sm:p-4">
+        {/* top bar */}
+        <div className="border-b-3 border-gray-300">
+          <ButtonGroup
+            variant="contained"
+            aria-label="Basic button group"
+            size="small"
+          >
+            <Button>Summary</Button>
+            <Button variant="outline">What-if</Button>
+          </ButtonGroup>
+        </div>
+
+        {/* cards for data visualization */}
+        <div className="flex flex-col gap-4 sm:flex-row sm:gap-4 justify-center items-center">
+          <Card key={1} data={opportunityData} heading={"opportunity count"} />
+          <Card key={2} data={acvData} heading={"ACV"} />
+        </div>
+
+        {/* tables */}
+        <div className="flex flex-col gap-4 sm:flex-row sm:gap-4 justify-center items-center">
+          <TableCard
+            key={1}
+            data={opportunityTableData}
+            totalValue={opportunityTotalValue}
+          />
+          <TableCard key={2} data={acvTableData} totalValue={acvTotalData} />
+        </div>
       </div>
 
-      {/* cards for data visualization */}
-      <div className="flex flex-col gap-4 sm:flex-row sm:gap-4 justify-center items-center">
-        <Card key={1} data={opportunityData} heading={"opportunity count"} />
-        <Card key={2} data={acvData} heading={"ACV"} />
-      </div>
-
-      {/* tables */}
-      <div className="flex flex-col gap-4 sm:flex-row sm:gap-4 justify-center items-center">
-        <TableCard key={1} data={opportunityTableData} totalValue={opportunityTotalValue}/>
-        <TableCard key={2} data={acvTableData} totalValue={acvTotalData}/>
-      </div>
+      <Footer />
     </div>
   );
 };
---
+++ frontend/src/component/Footer.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { FaGithub, FaTwitter, FaLinkedin } from "react-icons/fa";
+
+const Footer = () => {
+  const year = new Date().getFullYear();
+
+  return (
+    <footer className="border-t border-gray-200 py-6 bg-gray-100 text-gray-700">
+      <div className="container mx-auto px-6 flex flex-col md:flex-row justify-between items-center text-center md:text-left">
+        <div className="mb-4 md:mb-0">
+          <h2 className="text-2xl font-bold">
+            Skygeni
+          </h2>
+          <p className="text-sm">© {year} Skygeni. All rights reserved.</p>
+          <p className="text-sm text-gray-700">
+            Developed by{" "}
+            <a
+              href="https://github.com/kraarush"
+              className="text-blue-500 hover:text-blue-700 hover:font-medium"
+            >
+              Aarush Kumar
+            </a>
+          </p>
+        </div>
+
+        <div className="flex space-x-6">
+          <a
+            href="https://github.com/kraarush/skygeni_project"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-gray-500"
+          >
+            <FaGithub size={24} />
+          </a>
+          <a
+            href="https://twitter.com/AarushKr_"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-gray-500"
+          >
+            <FaTwitter size={24} />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/aarush-kr/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-gray-500"
+          >
+            <FaLinkedin size={24} />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
